6d1346018330f355eb49b6ceabfbf5b998c29778
SuperDuper342 <lawrencemilo342@gmail.com>
2024-05-02 14:58:11 -0600

    try

--- src/cv2Test.py
+++ src/cv2Test.py
@@ -1,23 +1,34 @@
-import cv2
-import pandas as pd
-import numpy as np
-from PIL import Image
+# importing libraries 
+import cv2 
+import numpy as np 
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture('../test/testFile.mp4')
+# Create a VideoCapture object and read from input file 
+cap = cv2.VideoCapture('src/test/testFile.mp4') 
 
-    while True:
-        ret, frame = cap.read()
+# Check if camera opened successfully 
+if (cap.isOpened()== False): 
+	print("Error opening video file") 
 
-        print(frame.shape())
+# Read until video is completed 
+while(cap.isOpened()): 
+	
+# Capture frame-by-frame 
+	ret, frame = cap.read() 
+	if ret == True: 
+	# Display the resulting frame 
+		cv2.imshow('Frame', frame) 
+		
+	# Press Q on keyboard to exit 
+		if cv2.waitKey(25) & 0xFF == ord('q'): 
+			break
 
-        image = cv2.resize(frame, (640, 480))
+# Break the loop 
+	else: 
+		break
 
-        cv2.imshow('Video', image)
+# When everything done, release 
+# the video capture object 
+cap.release() 
 
-        key = cv2.waitKey(1)
-        if key == 27:  # esc
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+# Closes all the frames 
+cv2.destroyAllWindows() 
