e5578e7c267c5d5b3540a12475da4f265a7d4935
SuperDuper342 <lawrencemilo342@gmail.com>
2024-05-02 14:49:10 -0600

    image resize

--- src/cv2Test.py
+++ src/cv2Test.py
@@ -1,6 +1,7 @@
 import cv2
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture('src/test/testFile.mp4')
@@ -8,7 +9,10 @@ if __name__ == "__main__":
     while True:
         ret, frame = cap.read()
 
-        cv2.imshow('Video', frame)
+        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        image = image.resize((640, 480))
+
+        cv2.imshow('Video', image)
 
         key = cv2.waitKey(1)
         if key == 27:  # esc
