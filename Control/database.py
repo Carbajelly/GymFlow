996f487b48f74270820b69ee080264bcbee4144f
Carbajelly <djcarb@colostate.edu>
2024-05-04 23:21:34 -0600

    oops

--- Control/database.py
+++ Control/database.py
@@ -9,16 +9,16 @@ class BenchTracker:
     def update_input(self, input_value):
         with self.lock:
             if input_value != self.last_input:
-                self.input_count += 1
+                self.bench_count += 1
                 self.last_input = input_value
     
     def get_usage_count(self):
         with self.lock:
-            return self.input_count
+            return self.bench_count
     
     def reset_usage_count(self):
         with self.lock:
-            self.input_count = 0
+            self.bench_count = 0
     
     def get_last_input(self):
         with self.lock:
